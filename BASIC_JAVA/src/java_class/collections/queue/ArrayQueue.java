dae91654e58896d1a5679a88476493dbf34e7630
xxx-sj <loky13@daum.net>
2023-09-23 15:49:06 +0900

    poll method 추가

--- BASIC_JAVA/src/java_class/collections/queue/ArrayQueue.java
+++ BASIC_JAVA/src/java_class/collections/queue/ArrayQueue.java
@@ -7,6 +7,13 @@ public class ArrayQueue<E> implements Queue<E> {
     private Object[] array;
     private int size;
 
+    /**
+     * front와 rear의 개념은 다음과 같다.
+     * rear은 queue에서 가장 나중에 들어온 위치를 뜻한다.
+     * rear+1을 하게되면 다음 요소를 넣을 인덱스를 나타낸다.
+     * front는 queue에서 꺼낼 위치 - 1 인덱스를 가리킨다.
+     * front + 1을 하게되면 다음으로 떠낼 인덱스의 위치가 된다.
+     */
     private int front;
     private int rear;
 
@@ -63,11 +70,53 @@ public class ArrayQueue<E> implements Queue<E> {
             resize(array.length * 2);
         }
 
+        //rear를 다음 위치로 갱신한다. [다음 요소를 넣을 위치]
         rear = (rear + 1) % array.length;
 
+        //update 한 rear 인덱스에 value를 넣는다.
         array[rear] = value;
+        //요소 갯수를 하나 증가시킨다.
         size++;
 
         return true;
     }
+
+
+    /**
+     * poll 메서드는 remove와는 다르게 NoSuch exception을 
+     * 던지지 않고 null을 반환한다.
+     */
+    @Override
+    public E poll() {
+        
+        if(size == 0) return null;
+
+        // %array.length로 나누는 이유는 원형큐로 
+        front = (front + 1) % array.length; //front를 한칸 옮긴다.
+        
+
+        @SuppressWarnings("unchecked")
+        E item = (E) array[front]; //반환할 데이터를 임시 저장
+        
+        array[front] = null;
+        size--;
+
+        /**
+         * array.length > DEFAULT_CAPACITY 조건이 있는 이유는
+         * array.length가 적어도 DEFAULT_CAPACITY 보다 커야하기 때문이다.
+         * size < (array.length / 4) 인 이유는 배열의 크기는 크나, 내부에 있는
+         * 요소의 크기가 배열의 크기의 1/4 일 경우에 빈 공간이 많기 때문에 
+         * 줄여주기 위해서이다.
+         */
+
+        if(array.length > DEFAULT_CAPACITY && size < (array.length / 4)) {
+
+            resize(Math.max(DEFAULT_CAPACITY, array.length / 2));
+        }
+
+        return item;
+    }
+
+
+    
 }
