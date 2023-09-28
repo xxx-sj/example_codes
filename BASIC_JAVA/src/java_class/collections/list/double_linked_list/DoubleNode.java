fcb5311381de7022ba79ccf0582f0fd5ec41ad5a
loky13 <loky13@daum.net>
2023-09-28 18:40:24 +0900

    LinkedListDeque 구현 중

--- BASIC_JAVA/src/java_class/collections/list/double_linked_list/DoubleNode.java
+++ BASIC_JAVA/src/java_class/collections/list/double_linked_list/DoubleNode.java
@@ -2,12 +2,12 @@ package java_class.collections.list.double_linked_list;
 
 public class DoubleNode<E> {
 
-    E data;
-    DoubleNode<E> next;
-    DoubleNode<E> prev;
+    public E data;
+    public DoubleNode<E> next;
+    public DoubleNode<E> prev;
 
 
-    DoubleNode(E value) {
+    public DoubleNode(E value) {
         this.data = value;
         next = null;
         prev = null;
