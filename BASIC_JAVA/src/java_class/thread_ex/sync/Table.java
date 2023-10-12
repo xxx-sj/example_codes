b8ca5ecd102c481f03563c76c74aad83972c9c23
loky13 <loky13@daum.net>
2023-10-12 23:19:31 +0900

    notify, wait 추가

--- BASIC_JAVA/src/java_class/thread_ex/sync/Table.java
+++ BASIC_JAVA/src/java_class/thread_ex/sync/Table.java
@@ -1,6 +1,5 @@
 package java_class.thread_ex.sync;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class Table {
@@ -10,32 +9,58 @@ public class Table {
     private ArrayList<String> dishes = new ArrayList<>();
 
     public synchronized void add(String dish) {
-        if(dishes.size() >= MAX_FOOD) {
-            return;
+//        if(dishes.size() >= MAX_FOOD) {
+//            return;
+//        }
+//
+//        dishes.add(dish);
+//        System.out.println("Dishes: " + dishes.toString());
+
+        while(dishes.size() >= MAX_FOOD) {
+            String name = Thread.currentThread().getName();
+            System.out.println(name + " is waiting");
+            try {
+                wait(); //COOK 스레드를 기다리게 한다.
+                Thread.sleep(500);
+            } catch (InterruptedException e) {}
         }
 
         dishes.add(dish);
+        notify();
         System.out.println("Dishes: " + dishes.toString());
     }
 
-    public boolean remove(String dishName) {
+    public void remove(String dishName) {
         synchronized (this) {
+            String name = Thread.currentThread().getName();
+
+            //size가 0이면 무한정 기다리게된다.
             while(dishes.size() == 0) {
-                String name = Thread.currentThread().getName();
                 System.out.println(name + "is waiting");
 
-                try {Thread.sleep(500);} catch (InterruptedException e) {}
+                try {
+                    wait(); //CUST 스레드를 기다리게 한다.
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {}
             }
 
-            for(int i = 0; i < dishes.size(); i++) {
-                if(dishName.equals(dishes.get(i))) {
-                    dishes.remove(i);
-                    return true;
-                }
-            }
+            while(true) {
+                //0이 아니면 제거한다.
+                for(int i = 0; i < dishes.size(); i++) {
+                    if(dishName.equals(dishes.get(i))) {
+                        dishes.remove(i);
+                        notify();
+                        return;
+                    }
+                } //for end
 
+                try {
+                    System.out.println(name + " is waiting.");
+                    wait();
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {}
+            }// while end
         } //sync
-            return false;
     }
 
     public int dishNum() {
@@ -54,20 +79,23 @@ public class Table {
 
         public void run() {
             while(true) {
-                try {Thread.sleep(10);} catch (InterruptedException e) {}
+                try {Thread.sleep(100);} catch (InterruptedException e) {}
                 String name = Thread.currentThread().getName();
 
-                if(eatFood()) {
-                    System.out.println(name + " ate a " + food);
-                } else {
-                    System.out.println(name + " failed to eat . :(");
-                }
+//                if(eatFood()) {
+//                    System.out.println(name + " ate a " + food);
+//                } else {
+//                    System.out.println(name + " failed to eat . :(");
+//                }
+
+                table.remove(food);
+                System.out.println(name + " ate a " + food);
             }
         }
 
-        boolean eatFood() {
-            return table.remove(food);
-        }
+//        boolean eatFood() {
+//            return table.remove(food);
+//        }
     }
 
 
@@ -82,7 +110,7 @@ public class Table {
             while(true) {
                 int idx = (int)(Math.random() * table.dishNum());
                 table.add(table.dishNames[idx]);
-                try {Thread.sleep(100);} catch (InterruptedException e) {}
+                try {Thread.sleep(10);} catch (InterruptedException e) {}
             }
         }
     }
@@ -95,7 +123,7 @@ public class Table {
         new Thread(new Customer(table, "donut"), "CUST1").start();
         new Thread(new Customer(table, "burger"), "CUST2").start();
 
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         System.exit(0);
     }
 }
