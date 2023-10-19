66e5b57a1c943f6bbc3a631e9d30bf7607c8b4b9
loky13 <loky13@daum.net>
2023-10-19 16:09:18 +0900

    java optional

---
+++ BASIC_JAVA/src/java_class/optional_ex/all_ex/All_ex.java
@@ -0,0 +1,61 @@
+package java_class.optional_ex.all_ex;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+import java.util.OptionalInt;
+
+public class All_ex {
+    public static void main(String[] args) {
+        Optional<String> optStr = Optional.of("strd");
+        Optional<Integer> optInt = optStr.map(String::length);
+        System.out.println("optStr.get() = " + optStr.get());
+        System.out.println("optInt.get() = " + optInt.get());
+
+        Integer result1 = Optional.of("123")
+                .filter(x -> x.length() > 0)
+                .map(Integer::parseInt).get();
+
+        Integer result2 = Optional.of("")
+                .filter(x -> x.length() > 0)
+                .map(Integer::parseInt).orElse(-1);
+
+        System.out.println("result1 = " + result1);
+        System.out.println("result2 = " + result2);
+
+        Optional.of("456").map(Integer::parseInt)
+                .ifPresent(x -> System.out.printf("result3 = %d%n", x));
+
+        OptionalInt optInt1 = OptionalInt.of(0);
+        OptionalInt optInt2 = OptionalInt.empty();
+        System.out.println(optInt1.isPresent());
+        System.out.println("optInt2.isPresent() = " + optInt2.isPresent());
+
+        System.out.println(optInt1.getAsInt());
+        System.out.println(optInt2.getAsInt());
+
+        System.out.println("optInt1 = " + optInt1);
+        System.out.println("optInt2 = " + optInt2);
+
+        System.out.println("optInt1.equals(optInt2) = " + optInt1.equals(optInt2));
+
+        Optional<String> opt = Optional.ofNullable(null);
+        Optional<String> opt2 = Optional.<String>empty();
+        System.out.println("opt = " + opt);
+        System.out.println("opt2 = " + opt2);
+
+        System.out.println("opt.equals(opt2) = " + opt.equals(opt2));
+        int result3 = optStrToInt(Optional.of("123"), 0);
+        int result4 = optStrToInt(Optional.of(""), 0);
+
+        System.out.println("result3 = " + result3);
+        System.out.println("result4 = " + result4);
+    }
+
+    static int optStrToInt(Optional<String> optStr, int defaultValue) {
+        try {
+            return optStr.map(Integer::parseInt).get();
+        } catch (Exception e) {
+            return defaultValue;
+        }
+    }
+}
---
+++ BASIC_JAVA/src/java_class/optional_ex/basic_ex/Basic_Ex.java
@@ -0,0 +1,48 @@
+package java_class.optional_ex.basic_ex;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public class Basic_Ex {
+    public static void main(String[] args) {
+        Basic_Ex.createOptional();
+        Basic_Ex.getOptional();
+        Basic_Ex.methods();
+        Basic_Ex.methods_2();
+    }
+
+    public static void createOptional() {
+        Optional<String> opt1 = Optional.of("abc");
+        Optional<String> opt2 = Optional.of(new String("abc"));
+
+        //Optional<String> opt3 = Optional.of(null);
+        Optional<String> opt3 = Optional.ofNullable(null);
+
+//        Optional<String> opt4 = null;
+        Optional<String> opt4 = Optional.<String>empty();
+    }
+
+    public static void getOptional() {
+        Optional<String> optVal = Optional.of("abc");
+        String s1 = optVal.get();
+        String s = optVal.orElse("");
+
+//        String s2 = optVal.orElseGet(() -> new String());
+        String s2 = optVal.orElseGet(String::new);
+        String s3 = optVal.orElseThrow(() -> new NullPointerException());
+    }
+
+    public static void methods() {
+        Integer i = Optional.of("123")
+                .filter(x -> x.length() > 0)
+                .map(Integer::parseInt)
+                .orElse(-1);
+    }
+
+    public static void methods_2() {
+        boolean present = Optional.ofNullable(null).isPresent();
+
+        Optional.ofNullable("null").ifPresent((i) -> System.out.println(i));
+    }
+
+}
