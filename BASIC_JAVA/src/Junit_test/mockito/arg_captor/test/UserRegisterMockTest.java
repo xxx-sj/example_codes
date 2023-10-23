a8a8048295932ae92d5809b4b0332589552c98a5
loky13 <loky13@daum.net>
2023-10-23 23:36:18 +0900

    mockito 인자캡처

--- BASIC_JAVA/src/Junit_test/mockito/arg_captor/test/UserRegisterMockTest.java
+++ BASIC_JAVA/src/Junit_test/mockito/arg_captor/test/UserRegisterMockTest.java
@@ -28,15 +28,22 @@ public class UserRegisterMockTest {
     @DisplayName("가입하면 메일을 전송함")
     @Test
     void whenRegisterThenSendMail() {
-        //when
+        //when 회원가입시 EmailNotifier를 이용해 이메일을 전송한다.
         userRegister.register("id", "pw", "email@email.com");
 
+        //인자[String type]을 캡처하기위해 ArgumentCaptor의 객체를 만든다.
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
 
         //then
+        /**
+         * userRegister.register로 인해
+         * mockEmailNotifier#sendRegisterEmail메서드가 불려야 하며,
+         * 메서드가 호출 될 때 인자로 전달받는 것을 capter.capture()로 캡처한다.
+         */
         BDDMockito.then(mockEmailNotifier)
                 .should().sendRegisterEmail(captor.capture());
 
+        //이 후 capture() 한 인자는 getValue()로 꺼낼 수 있다.
         String realEmail = captor.getValue();
         Assertions.assertEquals("email@email.com", realEmail);
     }
